3c18c0c19871fa3315ca4f9c76b68233a250ee1a
Yagna Amballa <yagna.s.amballa@kipi.bi>
2023-02-19 16:09:05 +0530

    with multiple elements - used html tag to encapsulate all of it

--- index.js
+++ index.js
@@ -75,16 +75,29 @@
 
 function Page(){
     return (
-        <div>
-           <h2>Reasons why I am excited to learn React</h2>
-             <ol>
+        <html>
+            <header>
+                <nav>
+                    <img src="download.svg"></img>
+                </nav>
+            </header>
+            <h2>
+            Reasons why I am excited to learn React
+            </h2>
+            
+            <ol>
                  <li>Adds to my list of skills that i know</li>
                  <li>Adds to the breadth of my skills</li>
                  <li>It gets me delicious packages</li>
                  <li>I can afford a comfortable lifestyle with the package that react gets me</li>
                  <li>Makes me more hireable</li>
             </ol> 
-        </div>
+            <footer>
+                &#169; 2023 Amballa development. All rights reserved
+            </footer>
+
+        </html>
+        
     )
 }
 
